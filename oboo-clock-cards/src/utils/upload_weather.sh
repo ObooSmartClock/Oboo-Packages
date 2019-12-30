d27c6c52ffb0c4ae3efc5c549ae6290a24d2ef69
Bradley Ouellette <bradley.ouellette@prometheanworld.com>
2019-12-30 10:16:47 -0800

    Adding module support
    Fixes:
      Changed the relative path loading to be based on current working directory
    	This will make it easier for testing/development since the paths
    	follow where you are currently not where the binary is.
      Added in the example module functions from duktape. This will allow us to
    	load modules in the ORT.
      Left in the card-lib and weather api loads but will remove them in the next
    	pull request to keep things smaller and more managable.
      Added Modules loading from a ./modules folder relative to the js file we are
    	running in, or from a global ../modules folder relative to main binary.

--- oboo-clock-cards/src/utils/upload_weather.sh
+++ oboo-clock-cards/src/utils/upload_weather.sh
@@ -6,5 +6,5 @@ if [ "$1" == "" ]; then
 fi
 
 rsync -va weather-card/weather.js root@$1:/usr/bin/js/weather.js
-rsync -va oboo-runtime/js/yahooWeather.js root@$1:/usr/bin/js/yahooWeather.js
+rsync -va oboo-runtime/js/weatherAPI.js root@$1:/usr/bin/js/modules/weatherAPI.js
 rsync -va weather-card/img/bin/ root@$1:/usr/bin/img/
